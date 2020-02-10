065d536fbd7b9ee05197845886ee1f2c82e11cc6
Christopher Sartin <christopher.daniel.sartin@gmail.com>
2020-02-10 11:45:23 -0600

    Fixed Ratio Problem with patternRatio: 0.8 on a-scene

--- public/index.php
+++ public/index.php
@@ -1,17 +1,18 @@
 <!doctype HTML>
 <html>
 <script src="https://aframe.io/releases/1.0.0/aframe.min.js"></script>
-<script src="https://raw.githack.com/jeromeetienne/AR.js/2.1.8/aframe/build/aframe-ar.js"></script>
+<script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
   <body style='margin : 0px; overflow: hidden;'>
-    <a-scene embedded arjs>
+    <a-scene embedded arjs="patternRatio: 0.8; debugUIEnabled: false">
 
       <a-asset>
         <a-asset-item id="teapot" src="Assets/teapot.obj"></a-asset-item>
       </a-asset>
 
-      <a-marker-camera type='pattern' url='https://raw.githubusercontent.com/ChristopherSartin/BKBDAR2020/master/public/Assets/AR_Concert/MarkersAndImages/pattern-bkbd.patt'>
+      <a-marker-camera type="pattern" url="./Assets/AR_Concert/MarkersAndImages/pattern-bkbd.patt">
           <a-entity scale='0.2 0.2 0.2' obj-model="obj:#teapot"></a-entity>
       </a-marker-camera>
+
     </a-scene>
   </body>
 </html>
